fa35897e9d08192dbe65f72afe257ccbb4286ce0
gurpreet <singhgurpreet1212@gmail.com>
2021-12-30 02:30:45 +0530

    do animation using javascript

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-const fadeInList = () => {
-	const delay = 4;
-	const faqItems = document.querySelectorAll('.faq-list .faq-item');
-	faqItems.forEach((item, i) => {
-		item.style.animation = `fadein 1s linear ${delay + i}s forwards`;
-	});
-	const faqCardButton = document.querySelector('.faq-card button');
-	faqCardButton.style.animation = `fadein 1s linear ${
-		delay + faqItems.length
-	}s forwards`;
+const startAnimation = (elements, reset = false) => {
+	const setAnimation = (element, delay, iterationCount = 1) => {
+		element.style.animation = reset
+			? 'none'
+			: `fadein 1s linear ${delay}s ${iterationCount} forwards`;
+	};
+
+	let delay = 0;
+
+	for (const key in elements) {
+		const element = elements[key];
+		if (element.node.length) {
+			element.node.forEach((item, i) => {
+				item.style.opacity = 0;
+				setAnimation(item, delay++);
+			});
+		} else {
+			const count = element.count ? element.count : 1;
+			setAnimation(element.node, delay++, count);
+			element.node.style.opacity = 0;
+		}
+	}
+	// if (reset) startAnimation(elements);
 };
 
-fadeInList();
+const init = () => {
+	const elements = {
+		header: { node: document.querySelector('.chat-bot header') },
+		headerItems: { node: document.querySelector('.chat-bot header div') },
+		botLogo: { node: document.querySelector('.chat-bot .bot-logo'), count: 2 },
+		faqCard: { node: document.querySelector('.chat-bot .faq-card') },
+		faqItems: {
+			node: document.querySelectorAll('.chat-bot .faq-list .faq-item'),
+		},
+		faqCardButton: {
+			node: document.querySelector('.chat-bot .faq-card button'),
+		},
+	};
+	startAnimation(elements);
+	document.querySelector('.reset').addEventListener('click', () => {
+		startAnimation(elements, true);
+		setTimeout(() => startAnimation(elements), 500);
+	});
+};
+
+init();
